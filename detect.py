26dadac2f701204bb48f36cd70d971ca3742afe2
edgaremy <remy.edgar@gmail.com>
2024-10-09 14:53:48 +0200

    Updated detect script, with also clearer syntax

--- detect.py
+++ detect.py
@@ -3,18 +3,27 @@ from ultralytics import YOLO
 
 from dataloading import create_dataloader
 
-# Load a model
-model = YOLO("models/arthropod_dectector_wave11_best.pt") # load .pt file
+### PARAMETERS ###
+# Path to the YOLO model
+MODEL_PATH = "models/arthropod_dectector_wave16_best.pt"  # Replace with your model path
+FOLDER_PATH = "examples/Entomoscope_examples/"  # Replace with your input folder path
+OUTPUT_PATH = "./runs/detect"  # Replace with your desired output folder path
 
-# Load images from a folder
+CROP_PROFILE = "Entomoscope"  # Set to "None" for other image sources
+##################
+
+# Load model
+model = YOLO(MODEL_PATH) # load .pt file
+
+# Load images from input folder
 folder_path = "examples/Entomoscope_examples/"
-dataloader = create_dataloader(folder_path, crop_profile="Entomoscope")
+dataloader = create_dataloader(FOLDER_PATH, crop_profile=CROP_PROFILE)
 
 results = []
 for images, _, _ in dataloader:
 
     # Perform inference using YOLO model
-    predictions = model.predict(images, show=False, save=True, save_txt=False)
+    predictions = model.predict(images, show=False, save=True, project="./runs/detect", save_txt=False)
     # predictions = model(images)
     results.append(predictions)
 
@@ -25,6 +34,7 @@ total_detections = 0
 for result in results:
     # Process the predictions for each batch
     for prediction in result:
+        print(f"Number of detections: {prediction.boxes.cls.cpu().numpy().shape[0]}")
         total_detections += prediction.boxes.cls.cpu().numpy().shape[0]
 
 print(f"Total number of detections: {total_detections}")
